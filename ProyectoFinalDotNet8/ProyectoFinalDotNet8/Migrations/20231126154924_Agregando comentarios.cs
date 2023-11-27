255632c5504629bd43347d3aa438a97351a29154
AlexanderST01 <137325892+AlexanderST01@users.noreply.github.com>
2023-11-27 10:03:30 -0400

    Agregando campos al Register, R_Comentarios y otros pequeños cambios

---
+++ ProyectoFinalDotNet8/ProyectoFinalDotNet8/Migrations/20231126154924_Agregando comentarios.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoFinalDotNet8.Migrations
+{
+    /// <inheritdoc />
+    public partial class Agregandocomentarios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Comentarios",
+                columns: table => new
+                {
+                    ComentarioId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ComentarioTexto = table.Column<string>(type: "TEXT", nullable: false),
+                    Usuario = table.Column<string>(type: "TEXT", nullable: false),
+                    UsuarioId = table.Column<string>(type: "TEXT", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    MenuId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Nota = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comentarios", x => x.ComentarioId);
+                    table.ForeignKey(
+                        name: "FK_Comentarios_Menus_MenuId",
+                        column: x => x.MenuId,
+                        principalTable: "Menus",
+                        principalColumn: "MenuId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comentarios_MenuId",
+                table: "Comentarios",
+                column: "MenuId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comentarios");
+        }
+    }
+}
