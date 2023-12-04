807f60437c650aaddb918a5eac21f0b2ca8b055d
AlexanderST01 <137325892+AlexanderST01@users.noreply.github.com>
2023-12-04 09:45:36 -0400

    Agregando Observacion a Consulta Despacho para director y haciendo que cada vez que te logeas te redirija al Home

--- ProyectoFinalDotNet8/ProyectoFinalDotNet8/Migrations/20231204121456_Agregando observacion a despacho.Designer.cs
+++ ProyectoFinalDotNet8/ProyectoFinalDotNet8/Migrations/20231204121456_Agregando observacion a despacho.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProyectoFinalDotNet8.Data;
 
@@ -10,9 +11,11 @@ using ProyectoFinalDotNet8.Data;
 namespace ProyectoFinalDotNet8.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231204121456_Agregando observacion a despacho")]
+    partial class Agregandoobservacionadespacho
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.0");
---
+++ ProyectoFinalDotNet8/ProyectoFinalDotNet8/Migrations/20231204121456_Agregando observacion a despacho.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoFinalDotNet8.Migrations
+{
+    /// <inheritdoc />
+    public partial class Agregandoobservacionadespacho : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Observacion",
+                table: "Despachos",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodigoDistrital",
+                table: "CentrosEducativos",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Observacion",
+                table: "Despachos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodigoDistrital",
+                table: "CentrosEducativos",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
